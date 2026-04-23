2d77ce965113c674eac8f52289750d382ab862ed
Sanyam Shah <43819485+sanshah1211@users.noreply.github.com>
2026-04-24 01:21:56 +0530

    Update authentik_common.py

--- backend/services/authentik/authentik_common.py
+++ backend/services/authentik/authentik_common.py
@@ -1,6 +1,6 @@
-AUTHENTIK_HOST = "http://192.168.0.80:9000"
-PG_PASSWORD = "uANLzfRdpGirzFOp11C2bG+RC46NhLhfgfNARdmKOcqf//qV"
-AUTHENTIK_API_TOKEN = "xdTSifCuR2Vok6FJ96NnDnmv77ed4LK5YdxmIVPvULBRtq8cIytvNmfy48vM"
+AUTHENTIK_HOST = "http://[authentik_ip:9000]"
+PG_PASSWORD = "[db_password]"
+AUTHENTIK_API_TOKEN = "[token]"
 AUTHENTIK_HEADER = {
     "Authorization": f"Bearer {AUTHENTIK_API_TOKEN}",
     "Content-Type": "application/json",
